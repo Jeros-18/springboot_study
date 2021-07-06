1ada04781a48907d16b8c47515b248fb12edf190
小星尘 <jahui6690429@163.com>
2021-07-06 20:16:21 +0800

    p46

--- src/main/java/com/atjh/boot05webadmin/bean/User.java
+++ src/main/java/com/atjh/boot05webadmin/bean/User.java
@@ -1,7 +1,11 @@
 package com.atjh.boot05webadmin.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class User {
     private String username;
--- src/main/java/com/atjh/boot05webadmin/controller/TableController.java
+++ src/main/java/com/atjh/boot05webadmin/controller/TableController.java
@@ -1,7 +1,12 @@
 package com.atjh.boot05webadmin.controller;
 
+import com.atjh.boot05webadmin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class TableController {
@@ -12,7 +17,14 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model){
+        List<User> users = Arrays.asList(new User("zhangsan","123435"),
+                new User("lisi","1234567"),
+                new User("wangwu","12345678"),
+                new User("jahui","93749237"));
+
+        model.addAttribute("users",users);
+
         return "table/dynamic_table";
     }
 
